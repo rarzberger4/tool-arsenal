b58ef29f317b43b645c4b43776489b601acef3b0
rarzberger <arzberger98@gmail.com>
2023-06-29 14:22:37 +0200

    added simple programm to start.py

---
+++ start.py
@@ -0,0 +1,15 @@
+# start.py
+
+def main():
+    # Your application's main logic goes here
+    print("Welcome to My Application!")
+
+    # Check for updates
+    import update
+    update.check_and_apply_update()
+
+    # Continue with the rest of your application
+    # ...
+
+if __name__ == "__main__":
+    main()
